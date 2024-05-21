7e29006d3e386f70925c820d55f39bd592a73b0c
jagrutijadeja <jagrutijadeja842@gmail.com>
2024-05-21 13:28:31 +0100

    Selenium-week1-com-utimateqa

---
+++ src/com_utimateqa_homework/ChromeBrowser_ComUtimateqa.java
@@ -0,0 +1,84 @@
+package com_utimateqa_homework;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+/**
+ * Project-5 - ProjectName : com-utimateqa
+ * BaseUrl = https://courses.ultimateqa.com/
+ * 1. Setup chrome browser.
+ * 2. Open URL.
+ * 3. Print the title of the page.
+ * 4. Print the current url.
+ * 5. Print the page source.
+ * 6. Click on ‘Sign In’ link
+ * 7. Print the current url
+ * 8. Enter the email to email field.
+ * 9. Enter the password to password field.
+ * 10. Click on Login Button.
+ * 11. Navigate to baseUrl.
+ * 12. Navigate forward to Homepage.
+ * 13. Navigate back to baseUrl.
+ * 14. Refresh the page.
+ * 15. Close the browser.
+ */
+public class ChromeBrowser_ComUtimateqa {
+    public static void main(String[] args) {
+        String BaseUrl = "https://courses.ultimateqa.com/";
+
+        // 1. Setup chrome browser.
+        WebDriver driver = new ChromeDriver();
+
+        //2. Open URL.
+        driver.manage().window().maximize();
+        driver.get(BaseUrl);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+
+        //3. Print the title of the page.
+        System.out.println("The title of the page is: " + driver.getTitle());
+
+        // 4. Print the current url.
+        System.out.println("The current url of the page is:" + driver.getCurrentUrl());
+
+        //5. Print the page source.
+        System.out.println("The page source is:" + driver.getPageSource());
+
+        //6. Click on ‘Sign In’ link
+        driver.findElement(By.linkText("Sign In")).click();
+
+        //7. Print the current url
+        System.out.println("The Current url is: " + driver.getCurrentUrl());
+
+        //8. Enter the email to email field.
+        driver.findElement(By.id("user[email]")).sendKeys("jagrutijadeaj1717@gmail.com");
+
+        //9. Enter the password to password field.
+        driver.findElement(By.id("user[password]")).sendKeys("Jaymaadi@17");
+
+        //10. Click on Login Button.
+        WebElement loginBtn = driver.findElement(By.xpath("//button[contains(text(),'Sign in')]"));
+
+        //11. Navigate to baseUrl.
+        driver.navigate().to(BaseUrl);
+
+        //12. Navigate forward to Homepage.
+        driver.navigate().forward();
+
+        //13. Navigate back to baseUrl.
+        driver.navigate().back();
+
+        //14. Refresh the page.
+        driver.navigate().refresh();
+
+        //15. Close the browser.
+         driver.quit();
+
+
+    }
+
+}
